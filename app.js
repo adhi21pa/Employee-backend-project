28186eb9fad70e129d8b739ec3c2914d7554079c
adhi21pa <adithyapa21@gmail.com>
2024-06-11 16:18:14 +0530

    project complete

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
-const  express = require("express")
-const  mongoose =require("mongoose")
-const  cors =require("cors")
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const {employeemodel} = require("./models/Employee")
 const app = express()
 app.use(cors())
+app.use(express.json())
 
-app.get("/",(req,res) =>{
-    res.send("hello")
+mongoose.connect("mongodb+srv://adithyapa2003:adithya66@cluster0.g5hvjxm.mongodb.net/employeedb?retryWrites=true&w=majority&appName=Cluster0")
+
+app.post("/add",(req,res)=>{
+   let input = req.body
+   let employee=new employeemodel(input)
+   employee.save()
+   console.log(employee)
+   res.json({"status":"success"})
 })
-app.get("/contact",(req,res)=>{
-    res.send("Welcome to my contact page")
-})
-app.listen(8080,()=>{
-    console.log("server started")
-})
+app.get("/view",(req,res) => {
+    employeemodel.find().then(
+
+    (data)=>{
+        res.json(data)
+    }
+    ).catch(
+        (error)=>{
+            res.json(error)
+        }
+    )
+            
+        })
+
+
+app.listen(8080)
+console.log("server started")
